ae55735b400b0dcc586ab6bc46c19d378a693643
Greenfin17 <jmaple@yahoo.com>
2021-05-28 23:44:41 -0500

    adding contact info to footer

--- src/components/section/Footer.js
+++ src/components/section/Footer.js
@@ -4,6 +4,14 @@ import React from 'react';
 
 const Footer = () => (
   <footer className='page-footer'>
+    <ul className='ul-contact-footer'>
+      <li><i className='fa fa-envelope' aria-hidden='true'></i>
+        <a href="mailto:jmaple177@gmail.com">jmaple177@gmail.com</a></li>
+      <li><i className='fab fa-github'></i>
+        <a href='https://github.com/Greenfin17'>Github</a></li>
+      <li><i className='fab fa-linkedin-in'></i>
+        <a href='https://www.linkedin.com/in/john-maple-84590643'>LinkedIn</a></li>
+    </ul>
     <div>Icons made by <a href='https://www.flaticon.com/authors/pixel-perfect' title='Pixel perfect'>Pixel perfect</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a></div>
   </footer>
 );
